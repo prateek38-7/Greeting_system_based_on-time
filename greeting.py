dea8bc94976e6e9e74f2475d4c40964ea1f445b8
prateek38-7 <prateekals0@gmail.com>
2026-05-02 16:14:15 +0530

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -3,10 +3,10 @@ import time
 hour = int(time.strftime("%H"))
 
 if hour < 12:
-	print("Good morning sir,how can i help you")
+	print("Good morning ,how can i help you")
 	
 elif hour < 18:
-	print("Good afternoon sir,how can i help you")
+	print("Good afternoon ,how can i help you")
 	
 else:
 	print("it is night,how can i help you")
